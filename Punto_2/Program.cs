dc926f7b165b1ff517a6b8bbd8e19487aa7cd334
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-03-08 21:39:15 -0500

    Segunda actualizacion p2

--- Punto_2/Program.cs
+++ Punto_2/Program.cs
@@ -6,17 +6,24 @@ internal class Program
     {
         //Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. Un segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad constante de 100 km/h. ¿A qué hora el segundo tren alcanzará al primer tren? Mostrar esta hora en pantalla
 
-        double horaa,horab,vela,velb;
+        // solicitar datos
+        double horaa,horab,vela,velb,dif;
         Console.Write("Ingrese los dos primeros dígitos en horario militar");
-        Console.Write("Ingrese la hora de salida del tren A:") ;
+        Console.Write("\nIngrese la hora de salida del tren A:") ;
         horaa=Convert.ToDouble(Console.ReadLine());
-        Console.Write("Ingrese la hora de salida del tren B:");
+        Console.Write("\nIngrese la hora de salida del tren B:");
         horab = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Ingrese solo números enteros positivos para la velocidad");
-        Console.Write("Ingrese la velocidad de salida del tren A:");
+        Console.Write("\nIngrese solo números enteros positivos para la velocidad");
+        Console.Write("\nIngrese la velocidad de salida del tren A:");
         vela=Convert.ToDouble(Console.ReadLine());
-        Console.Write("Ingrese la velocidad de salida del tren B:");
+        Console.Write("\nIngrese la velocidad de salida del tren B:");
         velb = Convert.ToDouble(Console.ReadLine());
+        // Convertir de horas a minutos
 
+        horaa = horaa * 60;// Convertir a minutos
+        horaa = horab * 60;
+        dif= horab-horaa;// diferencia en tiempo tren B vs Tren A
+        double distancia = dif / (60 * vela);// Distancia entre los dos trenes
+        double alcanzar = distancia / (vela * velb);//
     }
 }
