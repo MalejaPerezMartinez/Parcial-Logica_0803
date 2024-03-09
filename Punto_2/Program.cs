71c7e857a3913dc2b0c041e0d991a1f250e65d56
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-03-08 21:54:39 -0500

    Cuarta actualziacion

--- Punto_2/Program.cs
+++ Punto_2/Program.cs
@@ -7,28 +7,31 @@ internal class Program
         //Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. Un segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad constante de 100 km/h. ¿A qué hora el segundo tren alcanzará al primer tren? Mostrar esta hora en pantalla
 
         // solicitar datos
-        double horaa,horab,vela,velb,dif;
+        long horaa,horab,vela,velb,dif;
         Console.Write("Ingrese los dos primeros dígitos en horario militar");
         Console.Write("\nIngrese la hora de salida del tren A:") ;
-        horaa=Convert.ToDouble(Console.ReadLine());
+        horaa=Convert.ToInt32(Console.ReadLine());
         Console.Write("\nIngrese la hora de salida del tren B:");
-        horab = Convert.ToDouble(Console.ReadLine());
+        horab = Convert.ToInt32(Console.ReadLine());
         Console.Write("\nIngrese solo números enteros positivos para la velocidad");
         Console.Write("\nIngrese la velocidad de salida del tren A:");
-        vela=Convert.ToDouble(Console.ReadLine());
+        vela= Convert.ToInt32(Console.ReadLine());
         Console.Write("\nIngrese la velocidad de salida del tren B:");
-        velb = Convert.ToDouble(Console.ReadLine());
+        velb = Convert.ToInt32(Console.ReadLine());
         // Convertir de horas a minutos
 
         horaa = horaa * 60;// Convertir a minutos
         horaa = horab * 60;
         dif= horab-horaa;// diferencia en tiempo tren B vs Tren A
-        double distancia = dif / (60 * vela);// Distancia entre los dos trenes
-        double alcanzar = distancia / (vela * velb);// Tiempo que tomara en alcanzar Tren A a B
-        double tiempt = horab + alcanzar;// minutos totales en alcanzar
+        long distancia = dif / (60 * vela);// Distancia entre los dos trenes
+        long alcanzar = distancia / (vela * velb);// Tiempo que tomara en alcanzar Tren A a B
+        long tiempt = horab + alcanzar;// minutos totales en alcanzar
 
-        // Conversion de minutos a horas y fraccion de hora
+        // Conversión de minutos a horas y fracción de hora
 
+        tiempt = tiempt / 60; // hora
+        alcanzar=
+        
 
 
     }
