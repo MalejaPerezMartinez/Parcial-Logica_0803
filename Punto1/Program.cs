f0cf76544ce80f01dac637173584b92184b88757
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-03-08 20:42:44 -0500

    Primer punto parcial

---
+++ Punto1/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un sueldo base de $XXXX. Además, recibe una comisión del 10% sobre el valor total de cada venta que realiza.
+
+        double v1, v2, v3, sueldo;
+        Console.Write("Ingrese el valor de su sueldo base:");
+        sueldo = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor de la primera venta:");
+        v1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor de la segunda venta:");
+        v2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor de la primera venta:");
+        v3 = Convert.ToDouble(Console.ReadLine());
+
+    }
+}
