9b29cbc9ecc42650198abc36c990a6603abc8908
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-03-08 20:50:06 -0500

    Segunda actualziacion

--- Punto1/Program.cs
+++ Punto1/Program.cs
@@ -4,7 +4,7 @@
     {
         //Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un sueldo base de $XXXX. Además, recibe una comisión del 10% sobre el valor total de cada venta que realiza.
 
-        double v1, v2, v3, sueldo;
+        double v1, v2, v3,totalv, sueldo,comi;
         Console.Write("Ingrese el valor de su sueldo base:");
         sueldo = Convert.ToDouble(Console.ReadLine());
         Console.Write("Ingrese el valor de la primera venta:");
@@ -13,6 +13,19 @@
         v2 = Convert.ToDouble(Console.ReadLine());
         Console.Write("Ingrese el valor de la primera venta:");
         v3 = Convert.ToDouble(Console.ReadLine());
+
+        totalv=(v1 + v2 + v3);
+        comi = totalv*0.10;
+        sueldo=comi+sueldo;
+
+        if(totalv>1000000)
+      
+            Console.Write("Gano Benecifio");
+
+        else
+        {
+            Console.Write("No Gano Beneficio");
+        }
 
     }
 }
